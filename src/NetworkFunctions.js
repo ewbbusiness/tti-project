6dff55ed2247d6e5f7f8b4a039d4db3a4c8763dd
Sean Yu <sean.xiaochen.yu@gmail.com>
2021-05-07 15:34:31 -0400

    this should work

--- src/NetworkFunctions.js
+++ src/NetworkFunctions.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 
 // let apiURL = 'https://ewb-tti.herokuapp.com/api/';
-let apiURL = 'https://da03f0ec80f7.ngrok.io';
+let apiURL = 'https://5f5550607e45.ngrok.io/api/';
 
 export async function getFakeData() {
   try {
